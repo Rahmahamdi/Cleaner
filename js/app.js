fcce5e0f79a649495bc4d236c96730b02fb654a5
Rahmahamdi <47861630+Rahmahamdi@users.noreply.github.com>
2022-10-04 10:11:38 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,41 @@
-const croix = document.querySelectorAll('.visible-pannel img');
-console.log(croix);
-const el = document.querySelector(".questions croix");
 
-croix.forEach(element => {
-element.addEventListener('click', function(){
-console.log(this.src);
-if(this.src.includes('cross')){
-this.src = '/asset/remove.svg';
-}
-else if (this.includes('remove')) {
-this.src= '/asset/cross.svg'; 
-}
+document.addEventListener("DOMContentLoaded",(e)=>{ //function main
+    const element = document.querySelector(".container-faq .active");
+    const elt = document.querySelectorAll(".questions");
+
+
+    let section = element.children[1].querySelector("p");
+    let sectionHeight = section.offsetHeight +20;
+
+    element.children[1].style.height=sectionHeight +"px";
+    for (let el of elt) {
+        el.addEventListener("click", function(){
+            const active = document.querySelector(".container-faq .active");
+            active.classList.remove("active");
+            active.children[1].style.height=0;
+            this.classList.add("active");
+            let section = this.children[1].querySelector("p");
+            let sectionHeight = section.offsetHeight +20;
+        
+            this.children[1].style.height=sectionHeight +"px";
+            
+        })
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
-} )
